ea7b73b5e66360796bfa7b06f871ca58e756781a
danilandreevic <72157317+danilandreevic@users.noreply.github.com>
2022-11-28 16:26:30 +0600

    Update settings.py

--- BastauSite/settings.py
+++ BastauSite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-7sgtg!ce7@*y#)fkwjr^87$*^i#nu83uz^cq=grek@k0af$sq!
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['danilineu.pythonanywhere.com',]
+ALLOWED_HOSTS = ['danilineu1.pythonanywhere.com',]
 
 
 
